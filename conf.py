062984c6ffe8e2fff6165f4d25c98beb6013b04a
venumadhavi23 <79440805+venumadhavi23@users.noreply.github.com>
2021-02-23 16:45:22 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git & github documentation'
+copyright = '2021, venumadhavi'
+author = 'venumadhavi'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
